b0d0c6ea61ac2e7d4ca8c31f79fcdb086b3d63f4
Vaishnu Kanna <vaishnukanna04@gmail.com>
2024-03-21 15:34:31 +0530

    navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,37 +1,23 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import * as data from './links.json';
-const linksString = JSON.stringify(data);
-const links = JSON.parse(linksString).links;
-
-type Link = {
-    label: string;
-    href: string;
-};
-
-const Links: React.FC<{ links: Link[] }> = ({ links }) => {
-    return (
-        <div className={styles['links-container']}>
-            {links.map((link: Link) => {
-                return (
-                    <div key={link.href} className={styles['link']}>
-                        <a href={link.href}>
-                            {link.label}
-                        </a>
-                    </div>
-                )
-            })}
-        </div>
-    )
-};
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 const Navbar: React.FC = () => {
     return (
         <div className={styles.navbar}>  
-            <div className={styles['logo-container']}>
-                Logo
-            </div>
-            <Links links={links} />
+            <img src={logo} alt='auction-logo' className={styles['logo']}/>
+            <div className={styles['links-container']}>
+            <Link to='/overview' className={styles['link']}>
+                Overview
+            </Link>
+            <Link to='/bids' className={styles['link']}>
+                My Bids
+            </Link>
+            <Link to='/products' className={styles['link']}>
+                Products
+            </Link>
+            </div>         
         </div>
 
     )
