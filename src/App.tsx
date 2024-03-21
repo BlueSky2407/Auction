e619ad770c8b8f292af12f4d03848c27f1ea7de4
Vaishnu Kanna <vaishnukanna04@gmail.com>
2024-03-21 20:52:01 +0530

    updated navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import {Routes, Route} from "react-router-dom";
 import MyBids from './pages/MyBids';
 import ProductList from './pages/ProductList';
 import UserProfile from './pages/UserProfile';
+import Home from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <Routes>
+      <Route path='/' element={<Home />}></Route>
           <Route path='/products' element={<ProductList />}></Route>
           <Route path='/bids' element={<MyBids />}></Route>
           <Route path='/profile' element={<UserProfile />}></Route>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h4>Overview</h4>
+            <p>Click on the logo to come back to home</p>
+        </div>
+    )
+}
+
+export default Home;
